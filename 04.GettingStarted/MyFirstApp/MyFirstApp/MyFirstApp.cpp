555e4d4dfb9a503c18a1308bd41d135d51f89d4e
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2023-02-06 11:01:59 +0200

    write my first c++ app

---
+++ 04.GettingStarted/MyFirstApp/MyFirstApp/MyFirstApp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	int favorite_number;
+
+	std::cout << "Please enter your favorite number:";
+
+	std::cin >> favorite_number;
+
+	std::cout << "That's my favorite number too!" << std::endl;
+
+	return 0;
+}
