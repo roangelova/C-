f99672e05b69cd928d26bc79f19bd3ad4688a48c
RoslavaAngelova <88967550+roangelova@users.noreply.github.com>
2023-02-09 17:22:07 +0200

    refactor code and complete coding challenge

--- 06.VariablesAndConstants/FranksCarpetCleaningService/FranksCarpetCleaningService/FranksCarpetCleaningService.cpp
+++ 06.VariablesAndConstants/FranksCarpetCleaningService/FranksCarpetCleaningService/FranksCarpetCleaningService.cpp
@@ -7,26 +7,34 @@ using namespace std;
 int main()
 {
 
-	const int price_per_room = 30;
+	const double price_per_small_room = 30.0;
+	const double price_per_large_room = 35.0;
+
+	int number_of_small_rooms{ 0 };
+	int number_of_large_rooms{ 0 };
+
+	const double tax = 1.06;
 
 	cout << "Hello, welcome to Frank's Carpet Claning Service! \n" << endl; 
-	cout << "How many rooms would you like cleaned?" << endl; 
 
-	int number_of_rooms{ 0 };
-	float tax = 1.06;
+	cout << "How many small rooms would you like cleaned?" << endl; 
+	cin >> number_of_small_rooms;
 
-	cin >> number_of_rooms;
+	cout << "How many large rooms would you like cleaned?" << endl;
+	cin >> number_of_large_rooms;
 
-	float estimated_cost{ 0.0 };
+	double estimated_cost{ 0.0 };
 
+	estimated_cost = ((number_of_small_rooms * price_per_small_room ) + (number_of_large_rooms * price_per_large_room));
 
-	estimated_cost = number_of_rooms * price_per_room;
-	float total_cost = estimated_cost * tax;
+	double total_cost = estimated_cost * tax;
 
 	cout << "======================================" << endl;
 	cout << "Estimate for carpet cleaning service: \n" << endl;
-	cout << "Number of rooms: " << number_of_rooms << endl;
-	cout << "Price per room: $" << price_per_room << endl;
+	cout << "Number of small rooms: " << number_of_small_rooms << endl;
+	cout << "Number of large rooms: " << number_of_large_rooms << endl;
+	cout << "Price per small room: $" << price_per_small_room << endl;
+	cout << "Price per large room: $" << price_per_large_room << endl;
 	cout << "Total cost: $" << estimated_cost << endl;
 	cout << "Tax included: $" << total_cost << endl;
 	cout << "======================================" << endl;
